70490cc5cb3e174b8641bef4d1bb57f960b2be74
Theodore Terdes <tterdes@exist.com>
2011-08-08 18:52:12 +0800

    Added trapping for bug, feature/task segregation and subtasking

--- app/controllers/task_boards_controller.rb
+++ app/controllers/task_boards_controller.rb
@@ -10,33 +10,52 @@ class TaskBoardsController < ApplicationController
 
   def show
     @statuses = IssueStatus.all(:order => "position asc")
-  
-    unless params[:version_id].nil?
-      @version = Version.find params[:version_id]
-  
-      @fixed_issues = @version.fixed_issues
-    
-    else #this condition is for the Product Backlogs(issues with no targeted version)
-      get_project
-      
-      @fixed_issues = Issue.find(:all, :conditions => {:project_id => @project.id, :fixed_version_id => nil})   
-    end
-  
     @version = Version.find params[:version_id]
-    
-    @features = @version.features
-    @tasks = @version.tasks
-
-    @features.delete_if.each do |f| 
-      @tasks << f if not f.children_here?
-    end
+    #unless params[:version_id].nil?
+     # @version = Version.find params[:version_id]
   
-    @tasks.reject!.each do |f|
-      unless f.parent.nil?
-        p = Issue.find f.parent.issue_from_id
-        f if p.fixed_version_id == @version.id and p.feature?
+      #@fixed_issues = @version.fixed_issues
+    
+    #else #this condition is for the Product Backlogs(issues with no targeted version)
+     # get_project
+      
+      #@fixed_issues = Issue.find(:all, :conditions => {:project_id => @project.id, :fixed_version_id => nil})   
+    #end
+    puts params[:board].to_i.eql? 1
+    puts params[:board]
+    puts params[:board].class
+    puts 1.class
+    
+    if params[:board].to_i.eql? 1 
+    #This part needs to be optimized 
+      @features = @version.features
+      @tasks = @version.tasks
+
+      @features.delete_if.each do |f| 
+        @tasks << f if not f.children_here?
       end
+  
+      @tasks.reject!.each do |f|
+        unless f.parent.nil?
+          p = Issue.find f.parent.issue_from_id
+          f if p.fixed_version_id == @version.id and p.feature?
+        end
+      end
+      @featured = @features.empty? ? false : true
+      @tasked = @tasks.empty? ? false : true
+      @error_msg = "There are no Features/Tasks for this version." if not @featured and not @tasked
+    elsif params[:board].to_i.eql? 2
+      @bugs = @version.bugs
+      
+      @bugged = @bugs.empty? ? false : true
+      @error_msg = "There are no Bugs for this version." if not @bugged
+    else
+    
+      puts "Actually, there's something very wrong going on here."
     end
+    
+    @error_msg = "There are no issues for this version." if @version.fixed_issues.empty?
+    
   end
   
   def update_issue_status
