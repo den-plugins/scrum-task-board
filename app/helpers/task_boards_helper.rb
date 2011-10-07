14bf23be7ff253aea19dc8fd51e6b4ebc2eead78
lvillasica <lvillasica@lvillasica.(none)>
2011-10-07 09:52:35 +0800

    Include issue subject in tooltip.

--- app/helpers/task_boards_helper.rb
+++ app/helpers/task_boards_helper.rb
@@ -69,7 +69,8 @@ module TaskBoardsHelper
   end
   
   def task_board_tooltip(ticket)
-    content = "<strong>#{l(:field_description)}</strong>: #{ticket.description}<br />" +
+    content = "<strong>#{l(:field_subject)}</strong>: #{ticket.subject}<br />" +
+    "<strong>#{l(:field_description)}</strong>: #{ticket.description}<br />" +
     "<strong>#{l(:field_assigned_to)}</strong>: #{ticket.assigned_to}<br />" +
     "<strong>#{l(:field_remaining_effort)}</strong>: #{ticket.remaining_effort ? ticket.remaining_effort : 0} #{l(:field_sp_hours)}<br />" +
     "<strong>#{l(:field_comments)}</strong>: <br /> <ul>"
