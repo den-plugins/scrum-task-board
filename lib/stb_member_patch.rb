a355a6ffec00ab1ec3cfefef4311ed0e572eeddc
Normz <johnnormancapule@gmail.com>
2012-05-23 17:27:35 +0800

    Locations are now based on resource cost forecast allocations, not in the account settings

--- lib/stb_member_patch.rb
+++ lib/stb_member_patch.rb
@@ -16,17 +16,19 @@ module Stb
     def resource_day_capacity(week)
       days = 0
       allocations = resource_allocations
-      locations = [6]
-      locations << Holiday::LOCATIONS.select{|p,x| x == "#{user.location}"}.flatten[0]
-      locations << 3 if user.location.eql?('Cebu') || user.location.eql?('Manila')
 
       unless allocations.empty?
         week.each do |day|
           allocation = allocations.detect{ |a| a.start_date <= day && a.end_date >= day}
-          holiday = Holiday.find(:all, :conditions => ["event_date=? AND location IN (?)", day, locations])
-          if allocation && (1..5) === day.wday && holiday.empty?
-            div = (allocation.resource_allocation.to_f / 100)
-            days += (8 * div)
+          if allocation
+            locations = [6]
+            locations << allocation.location
+            locations << 3 if allocation.location == 2 || allocation.location == 1
+            holiday = Holiday.find(:all, :conditions => ["event_date=? AND location IN (?)", day, locations])
+            if (1..5) === day.wday && holiday.empty?
+              div = (allocation.resource_allocation.to_f / 100)
+              days += (8 * div)
+            end
           end
         end
       end
